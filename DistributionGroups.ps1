d50f0780db8a4188ad98c9efca915c9b9b1ba31d
Ben Snape <51653872+BenSnape@users.noreply.github.com>
2020-05-26 13:34:37 +0100

    Create DistributionGroups.ps1
    Taken from
    https://community.spiceworks.com/scripts/show/1388-export-distribution-list-members

---
+++ DistributionGroups.ps1
@@ -0,0 +1,13 @@
+#  Not mine - https://community.spiceworks.com/scripts/show/1388-export-distribution-list-members
+$saveto = "C:\disks\listmembers.txt"
+
+Get-DistributionGroup | sort name | ForEach-Object {
+
+	"`r`n$($_.Name)`r`n=============" | Add-Content $saveto
+	Get-DistributionGroupMember $_ | sort Name | ForEach-Object {
+		If($_.RecipientType -eq "UserMailbox")
+			{
+				$_.Name + " (" + $_.PrimarySMTPAddress + ")" | Add-Content $saveto
+			}
+	}
+}
